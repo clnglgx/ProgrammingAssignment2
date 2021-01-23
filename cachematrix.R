43b6db7d6e26f682d27170af2a8538d863f54e7c
clnglgx <77872901+clnglgx@users.noreply.github.com>
2021-01-23 13:46:59 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -13,3 +13,38 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+## This functions create a specail "matrix" that can chache its inverse
+## base on sample function makeVector and chacheVector
+
+## Create special "matrix" object
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set      = set, 
+       get      = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
+
+
+## Calculate matrix inverse, if stored skip calculation
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setsolve(m)
+  m
+}
